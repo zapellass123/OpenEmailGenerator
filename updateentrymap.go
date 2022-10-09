6a1abb72a093f30e1be1f36b45773956f05531d2
Manas3376 <72642953+Manas3376@users.noreply.github.com>
2022-10-09 12:50:53 +0530

    Updating an entry using map (Golang)

---
+++ updateentrymap.go
@@ -0,0 +1,16 @@
+package main
+
+import "fmt"
+
+func main() {
+
+	myGreeting := map[string]string{
+		"Tim":   "Good morning!",
+		"Jenny": "Bonjour!",
+	}
+
+	myGreeting["Harleen"] = "Howdy"
+	fmt.Println(myGreeting)
+	myGreeting["Harleen"] = "Gidday"
+	fmt.Println(myGreeting)
+}
