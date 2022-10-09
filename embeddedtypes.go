5735706d8cf8a0422014cafb6dc496d0ab9d2275
Manas3376 <72642953+Manas3376@users.noreply.github.com>
2022-10-09 12:55:45 +0530

    Added Embedded Type Code using Golang

---
+++ embeddedtypes.go
@@ -0,0 +1,39 @@
+package main
+
+import (
+	"fmt"
+)
+
+type person struct {
+	First string
+	Last  string
+	Age   int
+}
+
+type doubleZero struct {
+	person
+	LicenseToKill bool
+}
+
+func main() {
+	p1 := doubleZero{
+		person: person{
+			First: "James",
+			Last:  "Bond",
+			Age:   20,
+		},
+		LicenseToKill: true,
+	}
+
+	p2 := doubleZero{
+		person: person{
+			First: "Miss",
+			Last:  "MoneyPenny",
+			Age:   19,
+		},
+		LicenseToKill: false,
+	}
+
+	fmt.Println(p1.First, p1.Last, p1.Age, p1.LicenseToKill)
+	fmt.Println(p2.First, p2.Last, p2.Age, p2.LicenseToKill)
+}
