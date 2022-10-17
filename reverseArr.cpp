629657d34390001e9a601423c266b92af357ec9f
Chinmayee7697 <96012828+Chinmayee7697@users.noreply.github.com>
2022-10-17 12:37:28 +0530

    Update reverseArr.cpp

--- reverseArr.cpp
+++ reverseArr.cpp
@@ -1,17 +1,35 @@
 #include<iostream>
 using namespace std;
+
 int main()
 {
-    int arr[10], i;
-    cout<<"Enter 10 Array Elements: ";
-    for(i=0; i<10; i++)
+    int n,i;
+    
+    cout<<"Enter the number of elements in the array"<<endl;
+    cin>>n;
+    
+    int arr[n];
+    
+    //Entering the elements of array
+    cout<<"Enter "<<n<<" Array Elements: ";
+    
+    for(i=0; i<10; i++){
         cin>>arr[i];
+    }
+    
+    //Printing the elements of array
     cout<<"\nThe Original Array is:\n";
-    for(i=0; i<10; i++)
+    
+    for(i=0; i<10; i++){
         cout<<arr[i]<<" ";
+    }
+    
+    //Reversing the Array
     cout<<"\n\nThe Reverse of Given Array is:\n";
-    for(i=(10-1); i>=0; i--)
+    
+    for(i=(10-1); i>=0; i--){
         cout<<arr[i]<<" ";
-    cout<<endl;
+    }
+
     return 0;
 }
