49100cf37fee908177acb761e5478aaf43ab3d57
Faizansayeed28 <72259730+Faizansayeed28@users.noreply.github.com>
2022-10-21 19:32:25 +0530

    Implemented Addition of two binary numbers

---
+++ AddBinary.java
@@ -0,0 +1,17 @@
+class Solution {
+    public String addBinary(String a, String b) {
+        StringBuilder res = new StringBuilder();
+        int i = a.length() - 1;
+        int j = b.length() - 1;
+        int carry = 0;
+        while(i >= 0 || j >= 0){
+            int sum = carry;
+            if(i >= 0) sum += a.charAt(i--) - '0';
+            if(j >= 0) sum += b.charAt(j--) - '0';
+            carry = sum > 1 ? 1 : 0;
+            res.append(sum % 2);
+        }
+        if(carry != 0) res.append(carry);
+        return res.reverse().toString();
+    }
+}
